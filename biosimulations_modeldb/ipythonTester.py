9a6b8aee1d2367b99e54c2cc721eafb9e1f77b76
Briman Yang <82423465+BrmnYng@users.noreply.github.com>
2021-10-28 23:36:29 -0400

    Update ipythonTester.py

--- biosimulations_modeldb/ipythonTester.py
+++ biosimulations_modeldb/ipythonTester.py
@@ -1,10 +1,9 @@
-
+#if using ipython, enter lines 2 - 13 to get simulated version of code 
 import dateutil.parser
 import requests
 import yaml
 from collections import OrderedDict
 
-# get list of models
 response = requests.get('http://modeldb.science/api/v1/models?modeling_application=XPP')
 response.raise_for_status()
 model_ids = response.json()
@@ -12,6 +11,9 @@ model_ids = response.json()
 response = requests.get('http://modeldb.science/api/v1/models/279')  
 response.raise_for_status()
 model = response.json()
+
+
+#can be anything specific
 paper_id = model['model_paper']['value'][0]['object_id']
 citation_response = requests.get('http://modeldb.science/api/v1/papers/' + str(paper_id))
 citation_response.raise_for_status()
